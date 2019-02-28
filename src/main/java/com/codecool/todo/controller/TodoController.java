b313f7b75783dfb079267e182a416633806c90df
NoemiSz <szokenoemijob@gmail.com>
2019-02-28 18:48:16 +0100

    List repository items

--- src/main/java/com/codecool/todo/controller/TodoController.java
+++ src/main/java/com/codecool/todo/controller/TodoController.java
@@ -8,40 +8,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
-//@Controller
+
 @RestController
 public class TodoController {
 
     @Autowired
     private TodoRepository todoRepository;
 
+//List repository items
+    @RequestMapping("/list")
+    public List<Todo> todoList() {
+        return todoRepository.findAll();
+    }
 
-//    @RequestMapping("/")
-//    public String welcome(Model com.codecool.todo.model) {
-//        EntityDatabase entityDatabase = new EntityDatabase();
-//        com.codecool.todo.model.addAttribute("planets",PlanetCreator.planetMap);
-//        return "welcome";
-//    }
-//
-//    @GetMapping("/")
-//    public List<Todo> todoList(){
-//        return todoRepository.findAll();
-//    }
-//    public String todoList(Model model) {
-//        model.addAttribute("list", todoRepository.findAll());
-//        return "index";
-//    }
-//
-//    @RequestMapping("/planet")
-//    public String getForm(Model com.codecool.todo.model){
-//        return "planet";
-//    }
-//
-//    @PostMapping("/<planet_name>")
-//    public String getSize(@RequestParam(name="size", required=false) String name, Model com.codecool.todo.model) {
-//        com.codecool.todo.model.addAttribute("name", name);
-//        return "<planet name>";
-//    }
 
 }
